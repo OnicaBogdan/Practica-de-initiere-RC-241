26525ea823cd0f6bcb295d54fe76d858392816b6
OnicaBogdan <onicabogdan306@gmail.com>
2025-05-12 11:49:38 +0300

    Delete Login and Register directory

--- Login and Register/scripts/login.js
+++
@@ -1,5 +0,0 @@
-document.getElementById("loginForm").addEventListener("submit", function(event) {
-    event.preventDefault(); // Previne trimiterea formularului
-    // Redirectează la pagina principală
-    window.location.href = "main.html";
-});
