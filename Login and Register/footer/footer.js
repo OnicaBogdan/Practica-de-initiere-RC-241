68e0851e1d649eb059e7930d43bc792f81ed4b7e
OnicaBogdan <onicabogdan306@gmail.com>
2025-05-13 21:54:51 +0300

    Delete Login and Register directory

--- Login and Register/footer/footer.js
+++
@@ -1,8 +0,0 @@
-function loadFooter() {
-    fetch('../footer/footer.html')
-        .then(response => response.text())
-        .then(data => {
-            document.body.insertAdjacentHTML('beforeend', data);
-        });
-}
-loadFooter();
